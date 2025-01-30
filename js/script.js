4024a3a9547b82545ea3c9430800a752b266e646
icallejac <ignacio.calleja@elektra.com.mx>
2025-01-30 17:07:24 -0600

    Boton anterior

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-function galeriaImagenes (){
-    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
-        console.log("Entro a la 2")
+function galeriaImagenesSiguiente (){
+    if (document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach.jpg") {
         document.body.children[1].children[0].src = "./img/beach2.jpg"
-    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
-            console.log("Entro a la 3")
-            document.body.children[1].children[0].src = "./img/beach3.jpg"
-    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
-            console.log("Console 1")
-            document.body.children[1].children[0].src = "./img/beach.jpg"
+    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach2.jpg"){
+                document.body.children[1].children[0].src = "./img/beach3.jpg"
+    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach3.jpg"){
+                document.body.children[1].children[0].src = "./img/beach.jpg"
     } else {
-        console.log("Entro else")
-        document.body.children[1].children[0].src = "./img/beach2.jpg"
+                console.log("Error")
     }
 }
 
+function galeriaImagenesAnterior(){
+    if (document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach.jpg") {
+        document.body.children[1].children[0].src = "./img/beach3.jpg"
+    } else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach2.jpg"){
+        document.body.children[1].children[0].src = "./img/beach.jpg"
+    } else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach3.jpg"){
+        document.body.children[1].children[0].src = "./img/beach2.jpg"
+    } else {
+        console.log("Error")
+    }
+}
+
+function galeriaImagenesAuto(){
     
+}
