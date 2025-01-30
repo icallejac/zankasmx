c34c113e8387049814e40180584e19504a9ecb01
icallejac <ignacio.calleja@elektra.com.mx>
2025-01-30 15:40:53 -0600

    Boton siguiente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Hola Mundo en JS")
+function galeriaImagenes (){
+    if (document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach.jpg") {
+        console.log("Entro a la 2")
+        document.body.children[1].children[0].src = "./img/beach2.jpg"
+    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach2.jpg"){
+            console.log("Entro a la 3")
+            document.body.children[1].children[0].src = "./img/beach3.jpg"
+    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach3.jpg"){
+            console.log("Console 1")
+            document.body.children[1].children[0].src = "./img/beach.jpg"
+    } else {
+        console.log("Entro else")
+        document.body.children[1].children[0].src = "./img/beach2.jpg"
+    }
+}
+
+    
