e033ddbc238f994319c177d5d6d26b9011665a31
icallejac <ignacio.calleja@elektra.com.mx>
2025-01-30 17:13:55 -0600

    Corrige error rutas

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function galeriaImagenesSiguiente (){
-    if (document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach.jpg") {
+    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
         document.body.children[1].children[0].src = "./img/beach2.jpg"
-    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach2.jpg"){
+    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
                 document.body.children[1].children[0].src = "./img/beach3.jpg"
-    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach3.jpg"){
+    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
                 document.body.children[1].children[0].src = "./img/beach.jpg"
     } else {
                 console.log("Error")
@@ -11,11 +11,11 @@ function galeriaImagenesSiguiente (){
 }
 
 function galeriaImagenesAnterior(){
-    if (document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach.jpg") {
+    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
         document.body.children[1].children[0].src = "./img/beach3.jpg"
-    } else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach2.jpg"){
+    } else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
         document.body.children[1].children[0].src = "./img/beach.jpg"
-    } else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach3.jpg"){
+    } else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
         document.body.children[1].children[0].src = "./img/beach2.jpg"
     } else {
         console.log("Error")
@@ -23,5 +23,5 @@ function galeriaImagenesAnterior(){
 }
 
 function galeriaImagenesAuto(){
-    
+
 }
