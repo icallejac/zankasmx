138ce49d84333402526615e325170e4498573813
icallejac <ignacio.calleja@elektra.com.mx>
2025-01-30 15:47:54 -0600

    Corrige error  galeria

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function galeriaImagenes (){
-    if (document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach.jpg") {
+    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
         console.log("Entro a la 2")
         document.body.children[1].children[0].src = "./img/beach2.jpg"
-    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach2.jpg"){
+    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
             console.log("Entro a la 3")
             document.body.children[1].children[0].src = "./img/beach3.jpg"
-    }else if(document.body.children[1].children[0].src == "file:///Users/1033263/Documents/Develoments/zankasmx/img/beach3.jpg"){
+    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
             console.log("Console 1")
             document.body.children[1].children[0].src = "./img/beach.jpg"
     } else {
