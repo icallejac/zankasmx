1af20e2b3fc3095f42574aac8969fab4e2b307e5
icallejac <ignacio.calleja@elektra.com.mx>
2025-01-31 17:49:09 -0600

    Script galeria automatica

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function galeriaImagenesSiguiente (){
+/**function galeriaImagenesSiguiente (){
     if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
         document.body.children[1].children[0].src = "./img/beach2.jpg"
     }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
@@ -6,7 +6,7 @@ function galeriaImagenesSiguiente (){
     }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
                 document.body.children[1].children[0].src = "./img/beach.jpg"
     } else {
-                console.log("Error")
+            console.log("Error")
     }
 }
 
@@ -20,8 +20,18 @@ function galeriaImagenesAnterior(){
     } else {
         console.log("Error")
     }
+}*/
+
+function galeriaAuto(){
+    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
+       document.body.children[1].children[0].src = "./img/beach2.jpg"
+    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
+        document.body.children[1].children[0].src = "./img/beach3.jpg"
+    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
+        document.body.children[1].children[0].src = "./img/beach.jpg"
+    }else {
+        console.log("Error")
+    }
 }
 
-function galeriaImagenesAuto(){
-
-}
+setInterval(galeriaAuto, 5000)
