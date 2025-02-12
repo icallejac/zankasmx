dc2145ebf137ddca0ce8cfa00d816f2f1d856737
icallejac <ignacio.calleja@elektra.com.mx>
2025-02-12 13:20:00 -0600

    getelementid

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-/**function galeriaImagenesSiguiente (){
-    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
-        document.body.children[1].children[0].src = "./img/beach2.jpg"
-    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
-                document.body.children[1].children[0].src = "./img/beach3.jpg"
-    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
-                document.body.children[1].children[0].src = "./img/beach.jpg"
-    } else {
-            console.log("Error")
-    }
-}
-
-function galeriaImagenesAnterior(){
-    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
-        document.body.children[1].children[0].src = "./img/beach3.jpg"
-    } else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
-        document.body.children[1].children[0].src = "./img/beach.jpg"
-    } else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
-        document.body.children[1].children[0].src = "./img/beach2.jpg"
-    } else {
-        console.log("Error")
-    }
-}
-
-function galeriaAuto(){
-    if (document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
-       document.body.children[1].children[0].src = "./img/beach2.jpg"
-    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach2.jpg"){
-        document.body.children[1].children[0].src = "./img/beach3.jpg"
-    }else if(document.body.children[1].children[0].src == "https://icallejac.github.io/zankasmx/img/beach3.jpg"){
-        document.body.children[1].children[0].src = "./img/beach.jpg"
-    }else {
-        console.log("Error")
-    }
-}*/
-
 function galeriaAuto(){
     if (document.getElementById("gauto").src == "https://icallejac.github.io/zankasmx/img/beach.jpg") {
         document.getElementById("gauto").src = "./img/beach2.jpg"
